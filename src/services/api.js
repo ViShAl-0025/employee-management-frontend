61e0dd4ae4857a59945e0c3f4674868f12cd137a
ViShAl-0025 <vishalsingh8273952722@gmail.com>
2026-06-18 01:04:24 +0530

    Update API URL for production

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://employee-management-backend-fg8j.onrender.com/api",
 });
 
 export default api;
